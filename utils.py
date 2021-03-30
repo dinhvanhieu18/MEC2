a544a738851957b9412067c2cee17d53c50418b0
dinhvanhieu18 <kelvinhieu18@gmail.com>
2021-03-30 21:54:16 +0700

    update meanDelay with weight

--- utils.py
+++ utils.py
@@ -44,16 +44,24 @@ def update(message, network):
     index_list_car = -1
     index_list_rsu = -1
     index_list_receiveTime = -1
-    print("Update message ", message.stt)
-    for attr in dir(message):
-        print("obj.{} = {}".format(attr, getattr(message, attr)))
-    input()
+    # print("Update message ", message.stt)
+    # for attr in dir(message):
+    #     print("obj.{} = {}".format(attr, getattr(message, attr)))
+    # input()
     for i, location in enumerate(message.locations):
-        print("i=", i)
-        if i == len(message.locations) - 1 and not message.isDrop:
-            if len(message.locations) > 1:
-                print("Break")
+        # print("i=", i)
+        # ignore the last index car (had been updated before)
+        if len(message.indexCar) > 1:
+            if not message.isDrop and i == len(message.locations) - 1: 
+                # print("Break")
                 break
+        # ignor the last index rsu incase rsu1 ->rsu2 -> rsu1
+        if len(message.indexRsu) == 3:
+            if message.isDrop and i == len(message.locations) - 1:
+                break
+            if not message.isDrop and i == len(message.locations) - 2:
+                break
+    
         if location == 0:
             index_list_car += 1
             car_id = message.indexCar[index_list_car]
@@ -72,49 +80,45 @@ def update(message, network):
         # calculate delay for this object
         currentTime = message.currentTime
         delay = currentTime - receiveTime
-        print("delay=", delay)
-        for attr in dir(obj):
-            print("obj.{} = {}".format(attr, getattr(obj, attr)))
-        input()
+        # print("delay=", delay)
+        # for attr in dir(obj):
+        #     print("obj.{} = {}".format(attr, getattr(obj, attr)))
+        # input()
         # Check if message is drop, set delay to max delay in config
         obj.cnt += 1
         if message.isDrop:
             delay = Config.maxDelay
             obj.cntDrop += 1
-        obj.meanDelay += (delay - obj.meanDelay) / obj.cnt
+        # obj.meanDelay += (delay - obj.meanDelay) / obj.cnt
+        obj.meanDelay = Config.decayRateMean * obj.meanDelay + (1 - Config.decayRateMean) * delay
         obj.maxDelay = max(obj.maxDelay, delay)
         # Check if message is process by this object
         if (i == len(message.locations) - 2 and not message.isDrop) or \
            (i == len(message.locations) - 1):
             obj.cntProcess += 1
-            obj.meanDelayProcess += (delay - obj.meanDelayProcess) / obj.cntProcess
+            # obj.meanDelayProcess += (delay - obj.meanDelayProcess) / obj.cntProcess
+            obj.meanDelayProcess = Config.decayRateMean * obj.meanDelayProcess + (1 - Config.decayRateMean) * delay
         # else update with next location
         else:
             next_location = message.locations[i+1]
             if next_location == 0:
                 obj.cntSendToCar += 1
-                obj.meanDelaySendToCar += (delay - obj.meanDelaySendToCar) / obj.cntSendToCar
+                # obj.meanDelaySendToCar += (delay - obj.meanDelaySendToCar) / obj.cntSendToCar
+                obj.meanDelaySendToCar = Config.decayRateMean * obj.meanDelaySendToCar + (1 - Config.decayRateMean) * delay
             elif next_location == 1:
                 obj.cntSendToRsu += 1
-                obj.meanDelaySendToRsu += (delay - obj.meanDelaySendToRsu) / obj.cntSendToRsu
+                # obj.meanDelaySendToRsu += (delay - obj.meanDelaySendToRsu) / obj.cntSendToRsu
+                obj.meanDelaySendToRsu = Config.decayRateMean * obj.meanDelaySendToRsu + (1 - Config.decayRateMean) * delay
             else:
                 obj.cntSendToGnb += 1
-                obj.meanDelaySendToGnb += (delay - obj.meanDelaySendToGnb) / obj.cntSendToGnb
-        for attr in dir(obj):
-            print("obj.{} = {}".format(attr, getattr(obj, attr)))
-        input()
+                # obj.meanDelaySendToGnb += (delay - obj.meanDelaySendToGnb) / obj.cntSendToGnb
+                obj.meanDelaySendToGnb = Config.decayRateMean * obj.meanDelaySendToGnb + (1 - Config.decayRateMean) * delay
+        # for attr in dir(obj):
+        #     print("obj.{} = {}".format(attr, getattr(obj, attr)))
+        # input()
         # Update reward
         if location != 2:
-            print("Update reward")
-            print("Pre update")
-            for attr in dir(obj.optimizer):
-                print("obj.{} = {}".format(attr, getattr(obj.optimizer, attr)))
-            input()
             obj.optimizer.updateReward(message, delay)
-            print("After update")
-            for attr in dir(obj.optimizer):
-                print("obj.{} = {}".format(attr, getattr(obj.optimizer, attr)))
-            input()
 
         
         
