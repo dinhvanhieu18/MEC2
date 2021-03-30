df132528331faadcfb40222d9adfc8c4f8ab961c
dinhvanhieu18 <kelvinhieu18@gmail.com>
2021-03-30 21:53:20 +0700

    update optimizer interface

--- optimizers/optimizer.py
+++ optimizers/optimizer.py
@@ -1,8 +1,14 @@
 
 
 class Optimizer:
-    def __init__(self):
+    def __init__(self, agent_name, n_states, n_actions, policy_func):
         pass
 
-    def update(self):
+    def addToMemoryTmp(self, message, state, action):
+        pass
+
+    def updateReward(self, message, delay):
+        pass
+
+    def getAllActionValues(self, state):
         pass
